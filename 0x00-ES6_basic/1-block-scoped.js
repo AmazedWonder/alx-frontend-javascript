55b9bb414999d6843631d0517c838fc7b0c1deef
AmazedWonder <giftedwonda@gmail.com>
2024-10-03 23:36:34 +0100

    create 1-block-scoped.js and 1-main.js files to utilize javascript block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;// Changed var to let
-  let task2 = true;// Changed var to let
+  let task = false; // Changed var to let
+  let task2 = true; // Changed var to let
 
   if (trueOrFalse) {
-    task = true;// No var or let needed here, just assign
-    task2 = false;// No var or let needed here, just assign
+    task = true; // No var or let needed here, just assign
+    task2 = false; // No var or let needed here, just assign
   }
 
   return [task, task2];
