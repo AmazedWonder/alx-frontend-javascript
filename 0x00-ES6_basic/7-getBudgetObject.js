bc08241081852331778dc90a46c4163901d5c7ef
AmazedWonder <giftedwonda@gmail.com>
2024-10-04 01:17:04 +0100

    create .js files to modify object to simply use the keyname instead

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,9 @@
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income, // Shorthand property name
+    gdp, // Shorthand property name
+    capita, // Shorthand property name
+  };
+
+  return budget;
+}
