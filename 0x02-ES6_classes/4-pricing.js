972a943fbaf9b8dd6cc022e194585d8e9d4ff51b
AmazedWonder <giftedwonda@gmail.com>
2024-10-05 19:02:53 +0100

    create .js files to Implement a static method named convertPrice.

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,33 @@
+// 4-pricing.js
+
+/* eslint-disable no-underscore-dangle */
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(n) {
+    this._amount = n;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(n) {
+    this._currency = n;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
