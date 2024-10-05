de7dd4a6fb1b5f8f381a465955306afefeb935fa
AmazedWonder <giftedwonda@gmail.com>
2024-10-05 19:09:42 +0100

    create .js files to implement a method named evacuationWarningMessage.

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,19 @@
+// 5-building.js
+
+/* eslint-disable no-underscore-dangle */
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+}
